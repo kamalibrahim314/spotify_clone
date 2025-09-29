1251400032cb29f3603754619cbcc8a63d7bdcf6
kamal ibrahim <ibrahimkamal314@gmail.com>
2025-09-29 03:08:50 +0300

    api slice

--- src/redux/app/api/apiSlice.js
+++ src/redux/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import Cookies from 'js-cookie';
 
-const baseUrl = 'http://localhost:4000';
+const baseUrl = process.env.REACT_APP_API_URL;
 
 const baseQuery = fetchBaseQuery({
     baseUrl,
